6e2d4021cf62f1b7f18db92dc8e995131081d71b
Abinaya <abinaya.tce@gmail.com>
2022-11-06 17:58:09 +0100

    added operators

---
+++ code/Operators.py
@@ -0,0 +1,61 @@
+# # Arithmetic operators
+# # Assignment operators
+# # Comparision operators
+# # Logical operators
+# # Identity operators
+# # Membership operators
+# # Bitwise operators
+#
+# # Arithmetic operators
+x = 9
+y = 5
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+#
+# # Assignment operators
+#
+x = 10
+x += 2
+print(x)
+x -= 1
+print(x)
+x *= 2
+print(x)
+x /= 2
+print(x)
+x **= 5
+print(x)
+
+# Comparison operators
+x = 6
+y = 3
+print(x == y)
+print(x != y)
+print(x < y)
+print(x > y)
+print(x <= y)
+print(x >= y)
+
+# Logical operators
+x = 10
+y = 6
+print(x == y and x > y)
+print(x == y or x > y)
+
+# # Identity operators
+
+# x = ["abinaya, mohanraj"]
+# y = ["abinaya"
+#      ]
+
+
+
+
+
+# # Membership operators
+# # Bitwise operators
